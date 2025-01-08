583abf388334f0c32768900917b5fe65aa307c9c
Sebastian Kühnau <seb@vaadin.com>
2025-01-08 18:55:55 +0100

    add a route alias and make the view available with localhost:9966/petclinic/vets

--- src/main/java/org/springframework/samples/petclinic/ui/VetView.java
+++ src/main/java/org/springframework/samples/petclinic/ui/VetView.java
@@ -3,10 +3,12 @@ package org.springframework.samples.petclinic.ui;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import org.springframework.samples.petclinic.model.Vet;
 import org.springframework.samples.petclinic.service.ClinicService;
 
 @Route("")
+@RouteAlias("vets")
 public class VetView extends VerticalLayout {
     public VetView(ClinicService clinicService) {
         var grid = new Grid<>(Vet.class);
